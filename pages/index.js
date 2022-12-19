9c13cf03ae8553c539fc8afb712e1ff1ca7869f5
Fred Bliss <fbliss@gmail.com>
2022-12-19 15:41:20 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,9 @@ export default function Home() {
                 💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise
                 </a>
               </h2>
-              <p main className={styles.main}>
-                <a>Points of view and curated links across the intersection of AI, data, and B2B enterprise</a></p>
+              <div main className={styles.main}>
+                <a>Points of view and curated links across the intersection of AI, data, and B2B enterprise</a>
+              </div>
               <body>
                 <a href="https://post.news/fredbliss">post.news/fredbliss</a><br></br>
                 <a href="https://twitter.com/fblissjr">twitter.com/fblissjr</a><br></br>
