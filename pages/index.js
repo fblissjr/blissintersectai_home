77af951811653e720a6b8f4b2c6191e5317e6602
Fred Bliss <fbliss@gmail.com>
2022-12-19 14:57:44 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,15 +23,15 @@ export default function Home() {
               <h2 className={styles.description}>
                 <a>
                 🔎 CTO of all things data, intersection of B2B and AI
-🚀 Founder @ Aptitive (acquired by 2nd Watch)
-💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise
+                <br>🚀 Founder @ Aptitive (acquired by 2nd Watch)</br>
+                <br>💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise</br>
                 </a>
                 <a>Points of view and curated links across the intersection of AI, data, and B2B enterprise</a>
               </h2>
               <body>
-                <p><a href="https://post.news/fredbliss">post.news/fredbliss</a></p>
-                <p><a href="https://twitter.com/fblissjr">twitter.com/fblissjr</a></p>
-                <p><a href="https://linkedin.com/in/fbliss">https://linkedin.com/in/fbliss</a></p>
+                <br><a href="https://post.news/fredbliss">post.news/fredbliss</a></br>
+                <br><a href="https://twitter.com/fblissjr">twitter.com/fblissjr</a></br>
+                <br><a href="https://linkedin.com/in/fbliss">https://linkedin.com/in/fbliss</a></br>
               </body>
           </span>
         </p>
