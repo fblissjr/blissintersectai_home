2f2d3041d38184d230437c9c9b7d5676ba8e929c
Fred Bliss <fbliss@gmail.com>
2022-12-23 12:00:42 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,34 +7,27 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Fred Bliss</title>
-        <meta name="description" content="Fred Bliss" />
+        <meta name="description" content="Fred Bliss - CTO, founder, and AI/ML enthusiast" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a>Fred Bliss</a>
-        </h1>
-
-        <p className={styles.description}>
-          <span className={styles.description}>
-            <Image src="/profile.png"  alt="Bliss Intersect Logo" width={250} height={250}/>
-             
-              <h2 className={styles.description}>
-                <a>
-                🔎 CTO of all things data, intersection of B2B and AI<br></br>
-                🚀 Founder @ Aptitive (acquired by 2nd Watch)<br></br>
-                💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise
-                </a>
-                <p><a>Points of view and curated links across the intersection of AI, data, and B2B enterprise</a></p>
-              </h2>
-              <body>
-                <a href="https://post.news/fredbliss">post.news/fredbliss</a><br></br>
-                <a href="https://twitter.com/fblissjr">twitter.com/fblissjr</a><br></br>
-                <a href="https://linkedin.com/in/fbliss">https://linkedin.com/in/fbliss</a>
-              </body>
-          </span>
-        </p>
+        <div className={styles.header}>
+          <Image src="/profile.png" alt="Profile picture" width={250} height={250} />
+          <h1 className={styles.title}>Fred Bliss</h1>
+          <p className={styles.description}>
+            CTO of all things data, intersection of B2B and AI <br />
+            Founder @ Aptitive (acquired by 2nd Watch) <br />
+            AI/ML/LLM enthusiast, building in open, impacts on enterprise
+          </p>
+        </div>
+        <hr />
+        <p>Points of view and curated links across the intersection of AI, data, and B2B enterprise</p>
+        <div className={styles.socialLinks}>
+          <a href="https://post.news/fredbliss">post.news/fredbliss</a>
+          <a href="https://twitter.com/fblissjr">twitter.com/fblissjr</a>
+          <a href="https://linkedin.com/in/fbliss">https://linkedin.com/in/fbliss</a>
+        </div>
       </main>
 
       <footer className={styles.footer}>
