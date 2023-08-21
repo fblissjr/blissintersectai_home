6bcbc6588946e5119795deebbaeda974c32c322a
Fred Bliss <fbliss@gmail.com>
2023-08-21 11:19:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,46 +12,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <header className={styles.main}>
         <h1 className={styles.title}>
           <a>Fred Bliss</a>
         </h1>
+      </header>
 
-        <p className={styles.description}>
-          <span className={styles.description}>
-            <Image src="/profile.png"  alt="Bliss Intersect Logo" width={250} height={250}/>
-             
-              <h2 className={styles.description}>
-                <a>
-                🔎 CTO of all things data, intersection of B2B and AI<br></br>
-                🚀 Founder @ Aptitive (acquired by 2nd Watch)<br></br>
-                💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise
-                </a>
-        
-          <div style={{ padding: 30 }}>
+      <section className={styles.description}>
+        <Image src="/profile.png" alt="Bliss Intersect Logo" width={250} height={250} />
+
+        <h2 className={styles.subtitle}>
+          🔎 CTO of all things data, intersection of B2B and AI<br />
+          🚀 Founder @ Aptitive (acquired by 2nd Watch)<br />
+          💡 AI/ML/LLM enthusiast, building in open, impacts on enterprise
+        </h2>
+
+        <div className={styles.linkContainer}>
           <Link href="https://fredbliss.substack.com/?utm_source=home">
-            <a style={{
-              textDecoration: 'underline',
-              color: 'blue',
-            }}>
+            <a className={styles.newsletterLink}>
               Newsletter - Points of view and curated links across the intersection of AI, data, and B2B enterprise
             </a>
           </Link>
-          </div>
-              </h2>
-              <body>
-                <a href="https://linkedin.com/in/fbliss">LinkedIn (@fbliss)</a><br></br>
-                <a href="https://www.threads.net/@fblissjr">Threads (@fblissjr)</a><br></br>
-                <a href="https://twitter.com/fblissjr">Twitter (@fblissjr)</a><br></br>
-                <a href="https://post.news/fredbliss">Post (@fredbliss)</a>
-              </body>
-          </span>
-        </p>
-      </main>
+        </div>
 
-      <footer className={styles.footer}>
-        
-      </footer>
+        <div className={styles.contactLinks}>
+          <a href="https://linkedin.com/in/fbliss">LinkedIn (@fbliss)</a><br />
+          <a href="https://www.threads.net/@fblissjr">Threads (@fblissjr)</a><br />
+          <a href="https://twitter.com/fblissjr">Twitter (@fblissjr)</a><br />
+          <a href="https://post.news/fredbliss">Post (@fredbliss)</a>
+        </div>
+      </section>
+
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
